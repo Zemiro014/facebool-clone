fa0439e4bea3817b92b117c4f66829403387982c
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2023-01-26 22:19:41 -0300

    configure nextauth with facebook provider

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
+import Login from '@/components/Login';
+import { getSession } from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ session }) {
+  if(!session) return <Login />;
   return (
     <div>
       <Head>
@@ -18,4 +21,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // Get the user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
